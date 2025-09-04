57baf426ff432d38c9265d10b548a055892b5ffe
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-09-04 14:29:02 +0200

    feat: Refactor UserRegistrationListener to extract user creation logic into a separate method

--- keycloak/src/main/java/it/cnr/istc/coco/UserRegistrationListener.java
+++ keycloak/src/main/java/it/cnr/istc/coco/UserRegistrationListener.java
@@ -9,6 +9,8 @@ import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.events.admin.OperationType;
+import org.keycloak.events.admin.ResourceType;
 
 public class UserRegistrationListener implements EventListenerProvider {
 
@@ -20,26 +22,31 @@ public class UserRegistrationListener implements EventListenerProvider {
 
     @Override
     public void onEvent(Event event) {
-        if (EventType.REGISTER.equals(event.getType())) {
-            System.out.println("User registered: " + event.getUserId());
-            try (CloseableHttpClient client = HttpClients.createDefault()) {
-                HttpPost post = new HttpPost(WEBHOOK_URL + "/users");
-                post.setHeader("Content-Type", "application/json");
-                post.setEntity(new StringEntity("{\"keycloak_id\": \"" + event.getUserId() + "\"}"));
-                client.execute(post, response -> {
-                    if (response.getCode() != 201)
-                        throw new RuntimeException("Failed to create user. Response code: " + response.getCode());
-                    String user_id = EntityUtils.toString(response.getEntity());
-                    System.out.println("User created with ID: " + user_id);
-                    return null;
-                });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        if (EventType.REGISTER.equals(event.getType()))
+            create_user(event.getUserId());
     }
 
     @Override
     public void onEvent(AdminEvent event, boolean includeRepresentation) {
+        if (OperationType.CREATE.equals(event.getOperationType()) && ResourceType.USER.equals(event.getResourceType()))
+            create_user(event.getResourcePath().substring(event.getResourcePath().lastIndexOf('/') + 1));
+    }
+
+    private void create_user(String userId) {
+        System.out.println("User created with ID: " + userId);
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpPost post = new HttpPost(WEBHOOK_URL + "/users");
+            post.setHeader("Content-Type", "application/json");
+            post.setEntity(new StringEntity("{\"keycloak_id\": \"" + userId + "\"}"));
+            client.execute(post, response -> {
+                if (response.getCode() != 201)
+                    throw new RuntimeException("Failed to create user. Response code: " + response.getCode());
+                String user_id = EntityUtils.toString(response.getEntity());
+                System.out.println("User created with ID: " + user_id);
+                return null;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
